5e406a1ce1975d93dbd493e134100454c7d47b31
AdrielHigor <adrielhigor@gmail.com>
2019-08-17 13:49:19 -0300

    Update

--- api_join.py
+++ api_join.py
@@ -3,14 +3,22 @@ from requests.auth import HTTPBasicAuth
 #Simple API request and post algorithm 
 #Made by AdrielHigor
 
+class Contador(object):
+        def __init__(self, total):
+            self.total = total
+
+        def remains(self, done):
+                left = (100 * done)/self.total
+                return ("%.2f" % left)
+
 #Simple request on suap's API.
-api_request = requests.get('https://suap.ifpb.edu.br/api/ensino/alunos/v1/', auth=HTTPBasicAuth('user', 'password') )
+api_request = requests.get('https://suap.ifpb.edu.br/api/ensino/alunos/v1/', auth=HTTPBasicAuth('suap_login', 'suap_password') )
 
 #Getting response from 'api_request' as a json file.
 response_init = api_request.json()
 
 #API_ENDPOINT, show us where we are going to post our data responses in this case 'name', 'course', 'status', 'registration'
-API_ENDPOINT = ("http://10.3.132.172:8000/api/student/")
+API_ENDPOINT = ("http://localhost:8000/api/students/")
 
 #pagination varset
 offset = 0
@@ -18,11 +26,14 @@ offset = 0
 #Post count
 student_count = 0
 
+bytes = ((response_init["count"]))
+
+porcentagem = Contador(bytes)
 #A loop to pick all the responses from  the API and do the pagination based on the pages count of our API
 for page in range (((response_init["count"])//100)+1):
     
     #Dinamic page request
-    offset_request = requests.get('https://suap.ifpb.edu.br/api/ensino/alunos/v1/?offset={}'.format(offset), auth=HTTPBasicAuth('20161230008', 'Adr15025'))
+    offset_request = requests.get('https://suap.ifpb.edu.br/api/ensino/alunos/v1/?offset={}'.format(offset), auth=HTTPBasicAuth('suap_login', 'suap_password'))
     
     #Response handler
     response = offset_request.json()
@@ -39,10 +50,15 @@ for page in range (((response_init["count"])//100)+1):
                 'registration':student["matricula"]
                 }
 
-            student_post = requests.post(url = API_ENDPOINT, data=data) 
+            student_post = requests.post(url = API_ENDPOINT, data=data, headers = {'Authorization': 'token access_token_here'}) 
 
     offset += 100
     print('Page: ', offset)
-    print('Registered students: ', student_count)
+    print('Registred students: ', student_count)
+    print(porcentagem.remains(offset),'% complete;')
+    print('==============================================================')
 
-
+# name 
+# course
+# status
+# registration
